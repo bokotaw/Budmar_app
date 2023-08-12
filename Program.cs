d382525953818f5edd2cb16f154edd1f569256f8
bokotaw <70262538+bokotaw@users.noreply.github.com>
2023-08-12 19:45:01 +0200

    Connection string changes

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
 });
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
